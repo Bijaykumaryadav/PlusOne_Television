4ec0f939fb93a07459419b41ac2086a1bc31dedc
Bijay Kumar yadav <ybijayyadav468@gmail.com>
2026-02-19 23:14:32 +0545

    setting up apis

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,38 @@ const express = require("express");
 const app = express();
 const dbConnection = require("./config/db");
 const port = 8000;
+const passport = require("passport");
+const session = require("express-session");
+require("./middleware/passport-google-strategy");
+require("./middleware/passport-jwt-strategy");
+const cors = require("cors");
+const path = require("path");
 
+// for routes to accept the json files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL], // Allow specific origin from environment variable
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  optionsSuccessStatus: 200,
+  exposedHeaders: ["Content-Disposition", "Content-Type"],
+  allowedHeaders: ["Authorization", "Content-Type"], // Explicitly allow Authorization header
+  credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+};
+
+// Apply CORS middleware
+app.use(cors(corsOptions));
+
+
+//for using passport
+app.use(passport.initialize());
+app.use(passport.session());
 dbConnection();
+
+app.use("/apis/v1",require("./routes"));
+
 
 app.listen(port,() => {
     console.log(`Server is running on port ${port}`);
