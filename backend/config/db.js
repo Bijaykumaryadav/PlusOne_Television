f76e6ffe58b0d4c1a493ddba8f1c032db024e7f6
Bijay Kumar yadav <ybijayyadav468@gmail.com>
2026-08-31 12:49:33 +0545

    final commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const dbConnection = async() => {
+const dbConnection = async () => {
+  const mongoUri = process.env.MONGO_URL || "mongodb://mongo:27017/SidhaReporting";
 
-    try{
-        await mongoose.connect(process.env.MONGO_URL,{
-        dbName: "SidhaReporting",
+  try {
+    await mongoose.connect(mongoUri, {
+      dbName: "SidhaReporting",
     });
     console.log("Successfully Connected to the database");
-
-    }catch(error){
-        console.log("Error in creating the database",error);
-    }
-}
+  } catch (error) {
+    console.error("Error in creating the database:", error.message);
+  }
+};
 
 module.exports = dbConnection;
